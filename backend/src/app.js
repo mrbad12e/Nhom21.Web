a54100c999852538d0befaa0527ed8b0f80bbcf0
Đặng Kim Ngân <dkimngan1411@gmail.com>
2024-12-02 19:40:02 +0700

    update review

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
 const userRoutes = require('./routes/client/user.routes');
 const productRoutes = require('./routes/client/product.routes');
 const cartRoutes = require('./routes/client/cart.routes');
 const orderRoutes = require('./routes/OrderRoutes');
+const reviewRoutes = require('./routes/reviewRoutes'); 
 const app = express();
 
-// Middleware để parse dữ liệu JSON trong request body
-app.use(bodyParser.json());
-app.use('/auth', userRoutes);
-app.use('/api/cart', cartRoutes);
+// Middleware để parse dữ liệu JSON trong request body (tích hợp sẵn trong express)
+app.use(express.json());  
 
-// Dùng các routes cho yêu cầu đến /api/products
-app.use('/api/products', productRoutes);
+// Đăng ký các routes
+app.use('/auth', userRoutes);  // Route đăng nhập/đăng ký
+app.use('/api/cart', cartRoutes);  // Route giỏ hàng
+app.use('/api/products', productRoutes);  // Route sản phẩm
+app.use('/api/orders', orderRoutes);  // Route đơn hàng
+app.use('/api/reviews', reviewRoutes);  // Route  review
 
-app.use('/api/orders', orderRoutes);
 // Khởi động server
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
