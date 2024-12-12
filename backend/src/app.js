45ce1149ce40acb5f2573429012c28947efb24a3
mrbad12e <ntnghia9a7@gmail.com>
2024-12-12 23:02:55 +0700

    Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.raw({ limit: '100mb' }));
 app.set('trust proxy', 1);
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
